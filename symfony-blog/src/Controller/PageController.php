e518e991606fc20ef8a81515c5a1d034b2dd29cd
StefanKostenko <kostenkostephan@gmail.com>
2022-10-26 10:27:54 +0200

    Paso 2 finalizado

--- symfony-blog/src/Controller/PageController.php
+++ symfony-blog/src/Controller/PageController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactFormType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
@@ -23,10 +27,26 @@ class PageController extends AbstractController
     }
 
     #[Route('/contact', name: 'contact')]
-    public function contact(): Response
+    public function contact(ManagerRegistry $doctrine, Request $request): Response
     {
-        return $this->render('page/contact.html.twig', []);
+        $contact = new Contact();
+        $form = $this->createForm(ContactFormType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contacto = $form->getData();    
+            $entityManager = $doctrine->getManager();    
+            $entityManager->persist($contacto);
+            $entityManager->flush();
+            return $this->redirectToRoute('thankyou', []);
+        }
+        return $this->render('page/contact.html.twig', array(
+            'form' => $form->createView()    
+        ));
     }
 
+    #[Route('/thankyou', name:'thankyou')]
+    public function thankyou(): Response{
+        return $this->render('page/thankyou.html.twig', []);
+    }
 
 }
