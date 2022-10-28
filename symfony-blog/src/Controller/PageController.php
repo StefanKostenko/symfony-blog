7115c8efc28f99782d58d6192be59b6303a1857b
StefanKostenko <kostenkostephan@gmail.com>
2022-10-28 13:18:47 +0200

    admin finalizado

--- symfony-blog/src/Controller/PageController.php
+++ symfony-blog/src/Controller/PageController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Entity\Contact;
 use App\Form\ContactFormType;
 use Doctrine\Persistence\ManagerRegistry;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -12,10 +14,16 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/', name: 'index')]
-    public function index(): Response
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(ManagerRegistry $doctrine, Request $request): Response
     {
-        return $this->render('page/index.html.twig', []);
+        $repository = $doctrine->getRepository(Category::class);
+
+        $categories = $repository->findAll();
+
+        return $this->render('page/index.html.twig', ['categories' => $categories]);
     }
 
     /**
