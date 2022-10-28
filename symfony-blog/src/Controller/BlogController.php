265274095560ea1e4c621a005bd63fde44c9ec9c
StefanKostenko <kostenkostephan@gmail.com>
2022-10-28 13:53:46 +0200

    Paso 5.3

--- symfony-blog/src/Controller/BlogController.php
+++ symfony-blog/src/Controller/BlogController.php
@@ -2,21 +2,56 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Filesystem\Filesystem;
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Form\CommentFormType;
+use App\Form\PostFormType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
-
+use Symfony\Component\String\Slugger\SluggerInterface;
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'blog')]
-    public function index(): Response
+    /**
+     * @Route("/blog/{page}", name="blog")
+     */
+    public function index(ManagerRegistry $doctrine, int $page = 1): Response
     {
-        return $this->render('blog/blog.html.twig', []);
+        $repository = $doctrine->getRepository(Post::class);
+        $posts = $repository->findAllPaginated($page);
+        
+        return $this->render('blog/blog.html.twig', [
+            'posts' => $posts,
+        ]);
     }
 
-    #[Route('/single_post', name: 'single_post')]
-    public function single_post(): Response{
-        return $this->render('blog/single_post.html.twig', []);
+    /**
+     * @Route("/blog/new", name="new_post")
+     */
+    public function newPost(ManagerRegistry $doctrine, Request $request, SluggerInterface $slugger): Response
+    {
+        $post = new Post();
+        $form = $this->createForm(PostFormType::class, $post);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();   
+            $post->setSlug($slugger->slug($post->getTitle()));
+            $post->setPostUser($this->getUser());
+            $post->setNumLikes(0);
+            $post->setNumComments(0);
+            $entityManager = $doctrine->getManager();    
+            $entityManager->persist($post);
+            $entityManager->flush();
+            return $this->render('blog/new_post.html.twig', array(
+                'form' => $form->createView()    
+            ));
+        }
+        return $this->render('blog/new_post.html.twig', array(
+            'form' => $form->createView()    
+        ));
     }
-
 }
